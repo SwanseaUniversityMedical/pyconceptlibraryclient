1ca81acb4a5780bb664d15432cf7ff3daf7540c3
mr-possible <sambhavd4@gmail.com>
2023-07-04 21:12:32 +0100

    Update phenotype endpoint

--- utils/constants.py
+++ utils/constants.py
@@ -6,3 +6,5 @@ API_IGNORE_WRITE_FIELDS = [
     "updated",
 ]
 API_IGNORE_TEMPLATE_FIELDS = ["versions", "status", "is_deleted", "owner_access"]
+PATH_FOR_STORING_FILE_AFTER_UPDATE_PHENOTYPE = "./assets/gen/upload/update/"
+PATH_FOR_STORING_FILE_AFTER_CREATE_PHENOTYPE = "./assets/gen/upload/create/"
