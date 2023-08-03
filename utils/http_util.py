117315885d067aeaad0b00a041ee546360e95190
mr-possible <sambhavd4@gmail.com>
2023-08-03 21:20:40 +0100

    Used if-else instead of match in http_util

--- utils/http_util.py
+++ utils/http_util.py
@@ -5,19 +5,22 @@ def check_response(response):
     """
     Helper function to check the response and print accordingly.
     """
-    match response.status_code:
-        case 200:
-            print("Data Found!")
-            print_response(response)
-        case 201:
-            print("Data Created!")
-            print_response(response)
-        case 404:
-            print("Not found! Please Check!")
-        case 403:
-            print("Forbidden! This requires authentication!")
-        case 500:
-            print("Server Error!")
+    status_code = response.status_code
+    if status_code == 200:
+        print("Data Found!")
+        print_response(response)
+    elif status_code == 201:
+        print("Data Created!")
+        print_response(response)
+    elif status_code == 404:
+        print("Not found! Please Check!")
+    elif status_code == 403:
+        print("Forbidden! This requires authentication!")
+    elif status_code == 500:
+        print("Server Error!")
+    else:
+        print("Unknown status code:", status_code)
+
 
 def print_response(response):
     """
